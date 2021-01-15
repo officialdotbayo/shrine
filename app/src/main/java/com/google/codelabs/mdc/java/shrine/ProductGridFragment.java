4555a5f04b04f958779d0756a95fd3a22951a64b
officialdotbayo <74865725+officialdotbayo@users.noreply.github.com>
2021-01-15 13:48:24 +0100

    Initial commit

---
+++ app/src/main/java/com/google/codelabs/mdc/java/shrine/ProductGridFragment.java
@@ -0,0 +1,93 @@
+package com.google.codelabs.mdc.java.shrine;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.widget.NestedScrollView;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.codelabs.mdc.java.shrine.network.ProductEntry;
+
+public class ProductGridFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        // Inflate the layout for this fragment with the ProductGrid theme
+        View view = inflater.inflate(R.layout.shr_product_grid_fragment, container, false);
+
+        // Set up the toolbar
+        setUpToolbar(view);
+
+        // Set up the RecyclerView
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        GridLayoutManager grid_layout= new GridLayoutManager(getContext(),
+                2, GridLayoutManager.VERTICAL, false);
+        grid_layout.setSpanSizeLookup( new GridLayoutManager.SpanSizeLookup()
+    {
+
+        @Override
+        public int getSpanSize(int position)
+        {
+            return position % 3 == 2 ? 2 : 1;
+        }
+    });
+
+        recyclerView.setLayoutManager(grid_layout);
+        StaggeredProductCardRecyclerViewAdapter adapter = new StaggeredProductCardRecyclerViewAdapter(
+                ProductEntry.initProductEntryList(getResources()));
+        recyclerView.setAdapter(adapter);
+        int largePadding = getResources().getDimensionPixelSize(R.dimen.shr_product_grid_spacing);
+        int smallPadding = getResources().getDimensionPixelSize(R.dimen.shr_product_grid_spacing_small);
+        recyclerView.addItemDecoration(new ProductGridItemDecoration(largePadding, smallPadding));
+
+
+        // Set cut corner background for API 23+
+        NestedScrollView nested= view.findViewById(R.id.product_grid);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            nested.setBackgroundResource(R.drawable.shr_product_grid_background_shape);
+        }
+
+        return view;
+    }
+    private void setUpToolbar(View view) {
+        Toolbar toolbar = view.findViewById(R.id.app_bar);
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        if (activity != null) {
+            activity.setSupportActionBar(toolbar);
+        }
+
+        toolbar.setNavigationOnClickListener(
+                new NavigationIconClickListener(getContext(),
+                view.findViewById(R.id.product_grid)
+                , new AccelerateDecelerateInterpolator()
+                ,getContext().getResources().getDrawable(R.drawable.shr_branded_menu)
+                        ,getContext().getResources().getDrawable(R.drawable.shr_close_menu)));
+    }
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater menuInflater) {
+        menuInflater.inflate(R.menu.shr_toolbar_menu, menu);
+        super.onCreateOptionsMenu(menu, menuInflater);
+    }
+}
